c6d54eebf42f92dd9d6cc42be73900b10ea6a63e
xuzhiwei <jsrdxzw@163.com>
2019-06-30 15:54:59 +0900

    add test to compile byte code

---
+++ back-end/src/main/java/com/recruit/springdemo/handler/Test.java
@@ -0,0 +1,15 @@
+package com.recruit.springdemo.handler;
+
+/**
+ * @Author: xuzhiwei
+ * @Date: 2019-06-30
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 5;
+        int c = b - a;
+        System.out.println(c);
+    }
+}
